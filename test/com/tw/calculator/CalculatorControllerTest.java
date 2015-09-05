025a21bd78c05b36b32008b57371f39e136dcf68
pragya <pragya.91.jain@gmail.com>
2015-09-05 07:17:23 +0530

    Wrote a test for while loop to take input until exit

--- test/com/tw/calculator/CalculatorControllerTest.java
+++ test/com/tw/calculator/CalculatorControllerTest.java
@@ -1,7 +1,9 @@
 package com.tw.calculator;
 
 import org.junit.Before;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.contrib.java.lang.system.ExpectedSystemExit;
 
 import static org.mockito.Mockito.*;
 
@@ -12,33 +14,36 @@ public class CalculatorControllerTest {
     private Command command;
     private ExitCommand exitCommand;
 
+    @Rule
+    public final ExpectedSystemExit exit = ExpectedSystemExit.none();
+
     @Before
     public void setUp() {
         view = mock(View.class);
         parser = mock(Parser.class);
         calculator = mock(Calculator.class);
         command = mock(Command.class);
-        exitCommand = mock(ExitCommand.class);
+        CommandHistory commandHistory = mock(CommandHistory.class);
+        exitCommand = new ExitCommand(calculator, commandHistory, 0);
     }
 
     @Test
-    public void shouldDelegateToCalculatorController() {
-//        when(view.readInput()).thenReturn("add 5");
-//        when(parser.parse("add 5")).thenReturn(command);
-////        when(parser.parse("exit")).thenReturn(exitCommand);
-//
-//        doNothing().when(command).execute();
-//
-//        when(calculator.getAccumulator()).thenReturn((float) 5.0);
-//        doNothing().when(view).printResult((float) 5.0);
-//
-//        CalculatorController calculatorController = new CalculatorController(view, parser, calculator);
-//        calculatorController.start();
-//
-//        verify(view, times(1)).readInput();
-//        verify(parser).parse("add 5");
-////        verify(parser).parse("exit");
-//        verify(command).execute();
-//        verify(view).printResult((float) 5.0);
+    public void shouldReadInputAndParseAndExecute() {
+        when(view.readInput()).thenReturn("add 5").thenReturn("exit");
+        when(parser.parse("add 5")).thenReturn(command);
+        when(parser.parse("exit")).thenReturn(exitCommand);
+
+        exit.expectSystemExitWithStatus(0);
+        doCallRealMethod().when(calculator).exit(0);
+
+        CalculatorController calculatorController = new CalculatorController(view, parser, calculator);
+        calculatorController.start();
+
+        verify(view, times(2)).readInput();
+        verify(parser).parse("add 5");
+        verify(parser).parse("exit");
+        verify(command).addToHistory();
+        verify(command).execute();
+        verify(view).printResult((float) 5.0);
     }
 }
